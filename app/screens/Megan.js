a32a2629d4a18f4240e904329dd612f483299e55
Alvin Tan <alvin.tan1912@gmail.com>
2021-02-08 21:17:44 +0800

    Trying to send first request

--- app/screens/Megan.js
+++ app/screens/Megan.js
@@ -2,19 +2,44 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 
+const APIUrl = 'https://meg-backend-46.herokuapp.com/Megan/';
+
 function Megan() {
     const [messages, setMessages] = useState([]);
+
+    var initialMessage = '';
+    const data = JSON.stringify({
+        "stage": "1",
+        "response_type": "closed",
+        "text": "None"
+    })
+    fetch(APIUrl, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: data
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed.');
+    }, networkError => console.log(networkError.message)
+    ).then(jsonResponse => {
+        console.log(jsonResponse);
+        initialMessage = jsonResponse;
+    })
 
     useEffect(() => {
         setMessages([
             {
                 _id: 1,
-                text: 'Hello developer',
+                text: initialMessage,
                 createdAt: new Date(),
                 user: {
                     _id: 2,
-                    name: 'React Native',
-                    avatar: 'https://placeimg.com/140/140/any',
+                    name: 'Megan',
+                    avatar: require('C:/Users/atwb9/FYP/MEG/app/assets/logo.png'),
                 },
             },
         ])
