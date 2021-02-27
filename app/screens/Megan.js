5dfe7b220630f774432d7e2e763d9e48e82ca5f4
Alvin Tan <alvin.tan1912@gmail.com>
2021-02-27 21:33:24 +0800

    Megan basic functionality

--- app/screens/Megan.js
+++ app/screens/Megan.js
@@ -7,20 +7,21 @@ const APIUrl = 'https://meg-backend-46.herokuapp.com/Megan/';
 
 function Megan() {
     const [messages, setMessages] = useState([]);
+    const [userID, setUserID] = useState(Math.floor(Math.random() * 1000).toString())
 
     const onSend = useCallback((messages = []) => {
         setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
-        getReply(APIUrl, messages);
+        getReply(APIUrl, userID, messages[0]["text"]);
     }, []);
 
-    async function getReply(url, text) {
+    async function getReply(url, user_id, text) {
         const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json'
             },
             body: JSON.stringify({
-                "user_id": "1",
+                "user_id": user_id,
                 "function": "message",
                 "text": text
             })
@@ -45,43 +46,6 @@ function Megan() {
             return true;
         });
     }
-
-    async function sendMessage(url, text) {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                "user_id": "1",
-                "function": "message",
-                "text": text
-            })
-        }).then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('Request failed.');
-        }, networkError => console.log(networkError.message)
-        ).then(jsonResponse => {
-            let message = {
-                _id: Math.floor(Math.random() * 100),
-                text: jsonResponse,
-                createdAt: new Date(),
-                user: {
-                    _id: 2,
-                    name: 'Megan',
-                    avatar: require('C:/Users/atwb9/FYP/MEG/app/assets/MeganAvatar.png')
-                }
-            }
-            setMessages(previousMessages => GiftedChat.append(previousMessages, message));
-            return true;
-        });
-    }
-
-    useEffect(() => {
-        getReply(APIUrl);
-    })
 
     return (
         <View style={styles.container}>
